f0f92ae34de9a2c605bf5a324060d419f9aee09e
alexshavlovsky <ctzn@tut.by>
2019-04-06 16:59:15 +0300

    create model

---
+++ src/main/java/com/ctzn/springangularsandbox/model/Note.java
@@ -0,0 +1,59 @@
+package com.ctzn.springangularsandbox.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public class Note {
+    @Id
+    private UUID id;
+    private String title;
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Notebook notebook;
+
+    private Date lastModifiedOn;
+
+    private Note() {
+        id = UUID.randomUUID();
+        lastModifiedOn = new Date();
+    }
+
+    public Note(String title, String text, Notebook notebook) {
+        this();
+        this.title = title;
+        this.text = text;
+        this.notebook = notebook;
+    }
+
+    public Note(String id, String title, String text, Notebook notebook) {
+        this(title, text, notebook);
+        if (id != null) this.id = UUID.fromString(id);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Notebook getNotebook() {
+        return notebook;
+    }
+
+    public Date getLastModifiedOn() {
+        return lastModifiedOn;
+    }
+
+}
---
+++ src/main/java/com/ctzn/springangularsandbox/model/Notebook.java
@@ -0,0 +1,47 @@
+package com.ctzn.springangularsandbox.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+
+public class Notebook {
+    @Id
+    private UUID id;
+    private String name;
+
+    @OneToMany(mappedBy = "notebook", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Note> notes;
+
+    private Notebook() {
+        id = UUID.randomUUID();
+        notes = new ArrayList<>();
+    }
+
+    public Notebook(String name) {
+        this();
+        this.name = name;
+    }
+
+    public Notebook(String id, String name) {
+        this(name);
+        if (id != null) this.id = UUID.fromString(id);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+}
