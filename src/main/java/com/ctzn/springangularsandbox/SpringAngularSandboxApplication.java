40273a3df1bab6cd90f88178740fe691f1305593
alexshavlovsky <ctzn@tut.by>
2019-04-06 15:29:32 +0300

    A spring boot app created

---
+++ src/main/java/com/ctzn/springangularsandbox/SpringAngularSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.ctzn.springangularsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularSandboxApplication.class, args);
+	}
+
+}
