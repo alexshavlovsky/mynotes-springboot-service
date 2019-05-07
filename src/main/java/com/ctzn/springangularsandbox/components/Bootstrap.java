978fe1e3eb90e7f74134c8759a14f57db609b2e7
alexshavlovsky <ctzn@tut.by>
2019-05-07 23:09:07 +0300

    improve database initialisation using randomly generated notes

--- src/main/java/com/ctzn/springangularsandbox/components/Bootstrap.java
+++ src/main/java/com/ctzn/springangularsandbox/components/Bootstrap.java
@@ -8,6 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 @ConditionalOnProperty(name = "spring.jpa.hibernate.ddl-auto", havingValue = "create-drop")
 public class Bootstrap implements CommandLineRunner {
@@ -20,8 +22,20 @@ public class Bootstrap implements CommandLineRunner {
         this.noteRepository = noteRepository;
     }
 
-    @Override
-    public void run(String... args) {
+    private void generateRandomDb(int nbNum, int notesPerNbMin, int notesPerNbMax) {
+        Random rnd = new Random();
+        for (int i = 0; i < nbNum; i++) {
+            Notebook nb = new Notebook("Notebook " + (i + 1));
+            notebookRepository.save(nb);
+            int notesNum = rnd.nextInt(notesPerNbMax - notesPerNbMin + 1) + notesPerNbMin;
+            for (int n = 0; n < notesNum; n++) {
+                noteRepository.save(new Note("Note " + (i + 1) + "." + (n + 1),
+                        NonsenseGenerator.getInstance().makeText(1), nb));
+            }
+        }
+    }
+
+    private void generateStaticDb() {
         Notebook nb1 = new Notebook("Default");
         Notebook nb2 = new Notebook("Quotes");
         notebookRepository.save(nb1);
@@ -29,5 +43,11 @@ public class Bootstrap implements CommandLineRunner {
         noteRepository.save(new Note("Note 1", "Hello World", nb1));
         noteRepository.save(new Note("Hamlet", "To be or not to be", nb2));
         noteRepository.save(new Note("Terminator", "I'll be back", nb2));
+    }
+
+    @Override
+    public void run(String... args) {
+        generateRandomDb(12, 3, 40);
+        //generateStaticDb();
     }
 }
