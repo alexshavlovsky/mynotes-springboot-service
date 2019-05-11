86a6901f10bea4e9fe321929ded084b38b16642d
alexshavlovsky <ctzn@tut.by>
2019-05-11 21:24:52 +0300

    create a redirect from the home page to the swagger-ui.html

---
+++ src/main/java/com/ctzn/springangularsandbox/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.ctzn.springangularsandbox.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String index() {
+        return "redirect:swagger-ui.html";
+    }
+}
