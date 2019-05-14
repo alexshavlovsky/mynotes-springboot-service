2e022360c732e181be1dbcd5153af2fc19f37a70
alexshavlovsky <ctzn@tut.by>
2019-05-14 16:59:57 +0300

    forward all routes "/*" except "/*.*" to index.html (angular)

--- src/main/java/com/ctzn/springangularsandbox/controllers/HomeController.java
+++ src/main/java/com/ctzn/springangularsandbox/controllers/HomeController.java
@@ -1,12 +1,13 @@
 package com.ctzn.springangularsandbox.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-public class HomeController {
-    @RequestMapping(value = "/")
-    public String index() {
-        return "redirect:swagger-ui.html";
+public class HomeController { // forward all routes "/*" except "/*.*" to index.html
+    @GetMapping("/{angularRoute:^[^.]+$}")
+    public String index(@PathVariable String angularRoute) {
+        return "forward:/index.html";
     }
 }
