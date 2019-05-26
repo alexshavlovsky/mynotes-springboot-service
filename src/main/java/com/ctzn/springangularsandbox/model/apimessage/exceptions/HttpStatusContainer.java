888b110cf0f55fc86b797388c10ed86dfbcca2e2
alexshavlovsky <ctzn@tut.by>
2019-05-27 02:05:32 +0300

    refactor Exception handling architecture

---
+++ src/main/java/com/ctzn/springangularsandbox/model/apimessage/exceptions/HttpStatusContainer.java
@@ -0,0 +1,7 @@
+package com.ctzn.springangularsandbox.model.apimessage.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public interface HttpStatusContainer {
+    HttpStatus getHttpStatus();
+}
