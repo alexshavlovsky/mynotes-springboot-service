ff1597a44b9c87f8bc39cb6bf1a97f05b5b6c291
alexshavlovsky <ctzn@tut.by>
2019-05-29 22:44:39 +0300

    implement feedback controller test

---
+++ src/test/java/com/ctzn/mynotesservice/controllers/CommandControllerTest.java
@@ -0,0 +1,75 @@
+package com.ctzn.mynotesservice.controllers;
+
+import com.ctzn.mynotesservice.model.apimessage.ApiExceptionHandler;
+import com.ctzn.mynotesservice.model.apimessage.ApiMessage;
+import com.ctzn.mynotesservice.model.apimessage.TimeSource;
+import com.ctzn.mynotesservice.model.command.CommandController;
+import com.ctzn.mynotesservice.model.command.CommandRequest;
+import com.ctzn.mynotesservice.model.command.ShutdownManager;
+import com.ctzn.mynotesservice.repositories.DbSeeder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static com.ctzn.mynotesservice.controllers.RestTestUtil.mockPostRequest;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+public class CommandControllerTest {
+
+    private static final String BASE_PATH = CommandController.BASE_PATH;
+
+    @Mock
+    private DbSeeder dbSeeder;
+
+    @Mock
+    private ShutdownManager shutdownManager;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(new CommandController(dbSeeder, shutdownManager))
+                .setControllerAdvice(new ApiExceptionHandler())
+                .build();
+        TimeSource.setFixed(true);
+    }
+
+    @After
+    public void checkMocks() {
+        verifyNoMoreInteractions(dbSeeder, shutdownManager);
+    }
+
+    @Test
+    public void shouldAcceptAndExecuteShutdownCommand() throws Exception {
+        CommandRequest command = new CommandRequest("shutdown");
+        ApiMessage expected = new ApiMessage("Shutdown command accepted");
+        mockPostRequest(mockMvc, BASE_PATH, command, status().isAccepted(), expected);
+        verify(shutdownManager, times(1)).initiateShutdown(0);
+    }
+
+    @Test
+    public void shouldAcceptAndExecuteFillDatabaseCommand() throws Exception {
+        CommandRequest command = new CommandRequest("fill database");
+        ApiMessage expected = new ApiMessage("Fill database accepted");
+        mockPostRequest(mockMvc, BASE_PATH, command, status().isAccepted(), expected);
+        verify(dbSeeder, times(1)).run("force");
+    }
+
+    @Test
+    public void shouldRejectUnknownCommand() throws Exception {
+        CommandRequest command = new CommandRequest("smile");
+        ApiMessage expected = new ApiMessage("Command 'smile' not found");
+        mockPostRequest(mockMvc, BASE_PATH, command, status().isNotFound(), expected);
+    }
+
+}
